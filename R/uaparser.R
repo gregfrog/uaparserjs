fa4dfcc508bcc89897bfaba0fefa6c87f792bafc
Greg <greg@firmansyah.com>
2026-04-01 19:18:05 +1100

    update/add copyright notices

--- R/uaparser.R
+++ R/uaparser.R
@@ -1,3 +1,20 @@
+
+  #  Copyright 2020 Bob Rudis
+  #  Copyright 2026 Greg Hunt
+
+  #  Licensed under the Apache License, Version 2.0 (the "License");
+  #  you may not use this file except in compliance with the License.
+  #  You may obtain a copy of the License at
+
+  #      http://www.apache.org/licenses/LICENSE-2.0
+
+  #  Unless required by applicable law or agreed to in writing, software
+  #  distributed under the License is distributed on an "AS IS" BASIS,
+  #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  #  See the License for the specific language governing permissions and
+  #  limitations under the License.
+
+
 as_tibble <- function(.x) {
 
   out <- as.data.frame(.x, stringsAsFactors = FALSE)
@@ -18,7 +35,7 @@ as_tibble <- function(.x) {
 #'     plus patch level along with OS family and major & minor versions plus
 #'     device brand and model.
 #' @references <https://github.com/ua-parser/uap-core/>
-#' @note The regex YAML is now updated when the package is rebuilt.  The effective date is that of the package submission.
+#' @note The regex YAML from uap-core is now updated when the package is rebuilt.  The effective date can be found in the NEWS file.
 #' @examples
 #' ua_parse(paste0("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, ",
 #'                 "like Gecko) Ubuntu/11.10 Chromium/15.0.874.106 ",
--- R/uaparserjs-package.R
+++ R/uaparserjs-package.R
@@ -1,3 +1,18 @@
+#    Copyright 2020 Bob Rudis
+#    Copyright 2026 Greg Hunt
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 #' Parse 'User-Agent' Strings
 #'
 #' Despite there being a section in RFC 7231
@@ -6,6 +21,10 @@
 #' to parse consistently. Tools are provided that will take in user agent
 #' strings and return structured R objects. This is a 'V8'-backed package
 #' based on the 'ua-parser' project <https://github.com/ua-parser>.
+#' 
+#' The package incorporates regular expressions and test data from the ua parser project 
+#' that are copyrighted by Google and the UA Parser contributors and are used 
+#' under an Apache 2 license.  See the UA parser project for details.
 #'
 #' @name uaparserjs
 #' @author Bob Rudis (@@hrbrmstr)

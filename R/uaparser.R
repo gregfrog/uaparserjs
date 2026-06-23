c4f17b6eedc8376e80ab3b268c5485fcb720e172
Greg <greg@firmansyah.com>
2026-06-23 16:21:34 +1000

    ensure NA values are returned from parse and data cached correctly

--- R/uaparser.R
+++ R/uaparser.R
@@ -23,6 +23,16 @@ as_tibble <- function(.x) {
 
 }
 
+nullToNA <- function(x) {
+  lapply(x, function(x) {
+    if (is.list(x)){
+      nullToNA(x)
+    } else{
+      if(is.null(x)) NA else(x)
+    }
+  })
+}
+
 #' Parse a vector of user agents into a data frame
 #'
 #' Takes in a character vector of user agent strings and returns a data frame classed as tibble.
@@ -40,36 +50,97 @@ as_tibble <- function(.x) {
 #' ua_parse(paste0("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, ",
 #'                 "like Gecko) Ubuntu/11.10 Chromium/15.0.874.106 ",
 #'                 "Chrome/15.0.874.106 Safari/535.2", collapse=""))
-ua_parse <- function(user_agents, .progress=FALSE) {
+#'
 
+ua_parse <- function(user_agents, .progress=FALSE, useNA=FALSE) {
+
+  if(useNA) {
+    return(parseWithNA(user_agents, .progress=FALSE))
+  }
+  else
+  {
+
+    if (.progress) pb <- progress::progress_bar$new(length(user_agents))
+    if (.progress) pb$tick(0)
+
+    lapply(user_agents, function(x) {
+
+      cacheKey = paste0("1",x)
+
+      if (.progress) pb$tick()$print()
+
+      res <- .pkgenv$cache[[cacheKey]]
+
+      if (length(res) > 0) {
+
+        res
+
+      } else {
+
+        .pkgenv$cache[[cacheKey]] <- as_tibble(as.list(unlist(.pkgenv$ctx$call("parser.parse", x))))
+        .pkgenv$cache[[cacheKey]]
+
+      }
+
+    }) -> out
+
+    out <- bind_rows(out)
+
+    as_tibble(out)
+  }
+}
+
+parseWithNA <- function(user_agents, .progress=FALSE)
+{
   if (.progress) pb <- progress::progress_bar$new(length(user_agents))
   if (.progress) pb$tick(0)
 
+  if (is.null(user_agents))
+  {
+    user_agents = list(NULL)
+  }
+
   lapply(user_agents, function(x) {
+
+    cacheKey = paste0("0",x)
 
     if (.progress) pb$tick()$print()
 
-    res <- .pkgenv$cache[[x]]
+    cacheable = x != "" && !is.null(x) && !is.na(x)
 
-    if (length(res) > 0) {
-
-      res
-
-    } else {
-
-      .pkgenv$cache[[x]] <- as_tibble(as.list(unlist(.pkgenv$ctx$call("parser.parse", x))))
-      .pkgenv$cache[[x]]
-
+    if(is.null(x))
+    {
+      x = NA
     }
 
-  }) -> out
+    if(cacheable)
+    {
+      res <- .pkgenv$cache[[cacheKey]]
+      if (length(res) > 0)
+      {
+        return(res)
+      }
+    }
 
-  out <- bind_rows(out)
 
-  as_tibble(out)
+    wk2 = as.list(unlist(nullToNA(.pkgenv$ctx$call("parser.parse", x))))
+    if(cacheable)
+    {
+      .pkgenv$cache[[cacheKey]] = wk2
+    }
+    return(wk2)
+  }
+  ) -> out
+
+  out3 <- bind_rows(out)
+  return(as_tibble(out3))
 
 }
 
 #' @rdname ua_parse
 #' @export
 get_cache <- function() { .pkgenv$cache }
+
+cache_reset <- function() { .pkgenv$cache = list() }
+
+

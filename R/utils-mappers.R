1d461e2b83d3d03924ebba3b94e53e8050113da1
Greg <greg@firmansyah.com>
2026-06-23 16:17:56 +1000

    remove unused code

--- R/utils-mappers.R
+++ R/utils-mappers.R
@@ -14,20 +14,13 @@
 
 # this has limitations and is more like 75% of dplyr::bind_rows()
 # this is also orders of magnitude slower than dplyr::bind_rows()
-bind_rows <- function(..., .id = NULL) {
+bind_rows <- function(...) {
 
   res <- list(...)
 
   if (length(res) == 1) res <- res[[1]]
 
   cols <- unique(unlist(lapply(res, names), use.names = FALSE))
-
-  if (!is.null(.id)) {
-    inthere <- cols[.id %in% cols]
-    if (length(inthere) > 0) {
-      .id <- make.unique(c(inthere, .id))[2]
-    }
-  }
 
   id_vals <- if (is.null(names(res))) 1:length(res) else names(res)
 
@@ -41,10 +34,6 @@ bind_rows <- function(..., .id = NULL) {
         for (i in 1:length(moar_names)) {
           .x[[moar_names[i]]] <- rep(NA, length(.x[[1]]))
         }
-      }
-      if (!is.null(.id)) {
-        .x[[.id]] <- id_vals[idx]
-        idx <<- idx + 1
       }
       .x
     })
